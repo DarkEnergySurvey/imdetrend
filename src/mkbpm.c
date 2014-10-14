0f6e9224d9b58c18b0ea39d55f08d454646b0f4a
Alex Drlica-Wagner <kadrlica@fnal.gov>
2014-10-14 22:46:48 +0000

    Minor cleanup

--- src/mkbpm.c
+++ src/mkbpm.c
@@ -7,7 +7,9 @@
  *   Separate bias pixels into "hot" and "warm"
  *   Warm pixels are masked, hot pixels trigger masking the entire column
  *   Change defaults to loose tolerance on flats, 20 pixels at edge
- *
+ * 06/05/2014: A. Drlica-Wagner <kadrlica@fnal.gov>
+ *   Minor cleanup. Incorporated bugfix from Gary Bernstein.
+ *   Changed defaults displayed to be actual defaults.
  *
  * $Id$ 
  *
@@ -37,173 +39,107 @@
 
 int badcol(char *fname,int ccd,desimage out);
 
-
 int check_for_fits_list(char *filename,int *nim)
-
 {
-
   FILE *inp = NULL;
-
   char imagename[1000];
-
   char event[2000];
-
   int numim = 0;
-
   void reportevt();
 
   inp=fopen(filename,"r");
-
   if (inp==NULL) {
-
     sprintf(event,"File not found: %s",filename);
-
     reportevt(1,STATUS,5,event);
-
     exit(1);
-
   }
 
   while (fscanf(inp,"%s",imagename)!=EOF) {
-
     if (strncmp(&(imagename[strlen(imagename)-5]),".fits",5) &&
-
 	strncmp(&(imagename[strlen(imagename)-8]),".fits.gz",8)) {
-
       sprintf(event,"File must contain list of FITS or compressed FITS images");
-
       reportevt(1,STATUS,5,event);
-
       exit(1);
-
     }
 
     numim++;
-
   }
 
   if (fclose(inp)) {
-
     sprintf(event,"Input image list didn't close: %s",filename);
-
     reportevt(1,STATUS,5,event);
-
     exit(1);
-
   }
 
   if(nim)
-
     *nim = numim;
 
   return(numim);
-
 }
 
-
-
 int populate_list_from_file(char **list,char *filename)
-
 {
-
   FILE *inp = NULL;
-
   char imagename[1000];
-
   char event[2000];
-
   int numim = 0;
-
   void reportevt();
 
   inp=fopen(filename,"r");
 
   if (inp==NULL) {
-
     sprintf(event,"File not found: %s",filename);
-
     reportevt(1,STATUS,5,event);
-
     return(1);
-
   }
 
   while (fscanf(inp,"%s",imagename)!=EOF) {
-
     if(!list[numim]){
-
       sprintf(event,"List did not have enough space (%d), to hold list from %s.",numim,filename);
-
       reportevt(1,STATUS,5,event);
-
       return(1);
-
     }    
 
     sprintf(list[numim],"%s",imagename);
 
     numim++;
-
   }
 
   if (fclose(inp)) {
-
     sprintf(event,"Input image list didn't close: %s",filename);
-
     reportevt(1,STATUS,5,event);
-
     return(1);
-
   }
 
   return(0);
-
 }
 
 int mkbpm(int argc,char *argv[])
-
 {
-
-  
-
   static int status=0;
-
   void	printerror();
-
   int	i,j,hdutype,hdunum,flag_verbose=1,flag_bias=YES,edgesize=20,
-
     flag_output=YES,flag_edgemask=NO,flag_list=NO,imnum,imoutnum,im,
-
     mkpath(),flag_image_compare=NO,ncompare=0,ccdnum=0,xpos,ypos,badpix=0,flag_mask=NO;
 
   char	comment[200],imagename[500],longcomment[10000],
-
     event[10000];
 
   float	min=0.5,max=1.5,biashot=500.0,biaswarm=20.0,maxdev,rms,offset;
 
   desimage flat,bias,output,template;
-
   char **flatnames = NULL;
-
   char **biasnames = NULL;
-
   desimage *flatlist = NULL;
-
   desimage *biaslist = NULL;
 
   int nflat = 1;
-
   int nbias = 1;
-
   int initialize = 1;
 
   void	rd_desimage(),reportevt(),image_compare(),destroy_desimage(),headercheck();
-
   FILE	*inp,*out;
-
   time_t	tm;
-
   int doGeneric=0;
 
   int nbadcol;
@@ -230,23 +166,20 @@ int mkbpm(int argc,char *argv[])
 
   init_desimage(&template);
 
-
-
   if (argc<3) {
-
     printf("mkbpm <flatcor file | list> <biascor file | list> <bpm output> <options>\n");
 
     printf("  -flatmax <#,1.5>\n");
-    printf(" -numfmax <#,1>");
+    printf(" -numfmax <#,3>");
 
     printf("  -flatmin <#,0.5>\n");
     printf(" -numfmin <#,1>");
 
     printf("  -biashot <#,500>\n");
-    printf(" -numbhot <#,1>");
+    printf(" -numbhot <#,2>");
 
     printf("  -biaswarm <#,20>\n");
-    printf(" -numbwarm <#,1>");
+    printf(" -numbwarm <#,3>");
 
     printf("  -image_compare <template>\n");
 
@@ -263,197 +196,114 @@ int mkbpm(int argc,char *argv[])
     printf(" -doGeneric (set all BPM codes to BADPIX=1)");
 
     return(1);;
-
   }
 
-  
 
   /* ****************************************************************** */
-
   /* ****************** Extract verbose flag ************************** */
-
   /* ****************************************************************** */
-
-  
 
   for (i=4;i<argc;i++) 
-
     if (!strcmp(argv[i],"-verbose")) {
-
       sscanf(argv[++i],"%d",&flag_verbose);
-
       if (flag_verbose<0 || flag_verbose>3) {
-
 	sprintf(event,"Verbose level out of range %d . Reset to 2",
-
 		flag_verbose);
-
 	flag_verbose=2;
-
 	reportevt(2,STATUS,3,event);
-
       }
-
     }
 
-  
-
   /* ****************************************************************** */
-
   /* ************** Test input and output images ********************** */
-
   /* ****************************************************************** */
-
-  
 
   /* copy input flatcor image name if FITS file*/
 
   if (!strncmp(&(argv[1][strlen(argv[1])-5]),".fits",5) 
-
       || !strncmp(&(argv[1][strlen(argv[1])-8]),".fits.gz",8))  {
-
     flatnames    = (char **)calloc(1,sizeof(char *));
-
     flatnames[0] = (char *)calloc(1000,sizeof(char));
-
     sprintf(flatnames[0],"%s",argv[1]);
-
     imnum=1;
-
   }
-
   else if(check_for_fits_list(argv[1],&nflat)){
-
     flatnames = (char **)calloc(nflat,sizeof(char *));
-
     if (flag_verbose) {
-
       sprintf(event,"Input flat list %s contains %d FITS images",
-
 	      argv[1],nflat);
-
       reportevt(flag_verbose,STATUS,1,event);
-
     }
 
     for(i = 0;i < nflat;i++)
-
       flatnames[i] = (char *)calloc(1000,sizeof(char));
 
     if(populate_list_from_file(flatnames,argv[1])){
-
       sprintf(event,"mkbpm could not process input flat list: %s",argv[1]);
-
       reportevt(flag_verbose,STATUS,5,event);
-
       return(1);;
-
     }
-
   }
-
   else {
-
     sprintf(event,"mkbpm requires an input FITS flatcor image or list of images: %s",argv[1]);
-
     reportevt(flag_verbose,STATUS,5,event);
-
     return(1);;
-
   }
-
-  
 
   /* copy input biascor image name if FITS file*/
 
   if (!strncmp(&(argv[2][strlen(argv[2])-5]),".fits",5)
-
       || !strncmp(&(argv[2][strlen(argv[2])-8]),".fits.gz",8))  {
 
     biasnames = (char **)calloc(1,sizeof(char *));
-
     biasnames[0] = (char *)calloc(1000,sizeof(char));
-
     sprintf(biasnames[0],"%s",argv[2]);
-
     imnum=1;
-
   }
 
   else if(check_for_fits_list(argv[2],&nbias)){
-
     if (flag_verbose) {
-
       sprintf(event,"Input bias list %s contains %d FITS images",
-
 	      argv[2],nbias);
 
       reportevt(flag_verbose,STATUS,1,event);
-
     }
-
-    biasnames = (char **)calloc(nflat,sizeof(char *));
+    // ADW: Shouldn't this be nbias not nflat???
+    biasnames = (char **)calloc(nbias,sizeof(char *));
+    //biasnames = (char **)calloc(nflat,sizeof(char *));
 
     for(i = 0;i < nbias;i++)
-
       biasnames[i] = (char *)calloc(1000,sizeof(char));
 
     if(populate_list_from_file(biasnames,argv[2])){
-
       sprintf(event,"mkbpm could not process input bias list: %s",argv[2]);
-
       reportevt(flag_verbose,STATUS,5,event);
-
       return(1);
-
     }
-
   }
-
   else {
-
     sprintf(event,"mkbpm requires an input biascor image or list of images: %s",argv[2]);
-
     reportevt(flag_verbose,STATUS,5,event);
-
     return(1);;
-
   }
-
-  
 
   /* prepare output image */
 
-  
-
   if (!strncmp(&(argv[3][strlen(argv[3])-5]),".fits",5)){
-
     sprintf(output.name,"!%s",argv[3]);
-
     output.image=NULL;
-
     output.varim=NULL;
-
     output.mask=NULL;
-
     output.bitpix=USHORT_IMG;
-
   }
-
   else {
-
     sprintf(event,"mkbpm output must be FITS image: %s",argv[3]);
-
     reportevt(flag_verbose,STATUS,5,event);
-
     return(1);
-
   }
 
   /* ********************************************** */
-
   /* ********* PROCESS COMMAND LINE *************** */
-
   /* ********************************************** */
 
   for (i=4;i<argc;i++) {
@@ -471,8 +321,7 @@ int mkbpm(int argc,char *argv[])
     if (!strcmp(argv[i],"-numbhot")) sscanf(argv[i+1],"%i",&num_bias_hot);
     if (!strcmp(argv[i],"-numbwarm")) sscanf(argv[i+1],"%i",&num_bias_warm);
 
-     if (!strcmp(argv[i],"-doGeneric")) doGeneric=1;
- 
+    if (!strcmp(argv[i],"-doGeneric")) doGeneric=1;
 
     if (!strcmp(argv[i],"-respectmask")) flag_mask=YES;
 
@@ -485,19 +334,14 @@ int mkbpm(int argc,char *argv[])
     if (!strcmp(argv[i],"-image_compare"))  {
 
       flag_image_compare=YES;
-
       sprintf(template.name,"%s",argv[++i]);
 
       if (!strncmp(&(template.name[strlen(template.name)-5]),".fits",5)
-
 	  && !strncmp(&(template.name[strlen(template.name)-8]),".fits.gz",8))  {
 
 	sprintf(event,"Template image must be FITS: %s",template.name);
-
 	reportevt(flag_verbose,STATUS,5,event);
-
 	return(1);
-
       }
 
     }
@@ -507,21 +351,12 @@ int mkbpm(int argc,char *argv[])
   /* Issue event that contains the plan */
 
   if (flag_verbose) {
-
     sprintf(event,"Using flatcor (value>%.2f and value<%.2f) and biascor (>%.2f) to identify bad pixels",max,min,biashot);
-
     reportevt(flag_verbose,STATUS,1,event);
-
   }
 
-  
-
-  
-
   /* **************************************************** */
-
   /* *************** READING INPUT IMAGES *************** */
-
   /* **************************************************** */
 
   // *TODO: check ccdnum, hdutype
@@ -531,37 +366,26 @@ int mkbpm(int argc,char *argv[])
   for(j=0;j<nflat;j++){
 
     destroy_desimage(&flat);
-
     sprintf(flat.name,"%s",flatnames[j]);
-
     rd_desimage(&flat,READONLY,flag_verbose);
 
     if (flag_verbose) {
-
       sprintf(event,"Processing flat: %s",flat.name);
-
       reportevt(flag_verbose,STATUS,1,event);
-
     }
 
     /* check flat image */
 
     if (fits_movabs_hdu(flat.fptr,1,&hdutype,&status)) {
-
       sprintf(event,"Moving to HDU=1 failed: %s",flat.name);
-
       reportevt(flag_verbose,STATUS,5,event);
-
       printerror(status);
-
     }
 
     headercheck(&flat,"NOCHECK",&ccdnum,"DESMKFCR",flag_verbose);
 
     if(initialize==1){
-
       output.npixels=flat.npixels;
-
       output.nfound=flat.nfound;
 
       for (i=0;i<output.nfound;i++) output.axes[i]=flat.axes[i];
@@ -574,21 +398,15 @@ int mkbpm(int argc,char *argv[])
       warmtal = (int *) calloc(output.npixels,sizeof (int));
 
       if (output.mask==NULL) {
-
 	reportevt(flag_verbose,STATUS,5,"Calloc of output.mask failed");
-
 	return(1);
-
       }
 
       for (i=0;i<output.npixels;i++) {
-
 	output.mask[i] = 0;
-
       }
 
       initialize = 0;
-
     }
 
     /* create the BPM */
@@ -602,9 +420,10 @@ int mkbpm(int argc,char *argv[])
 
     if(flat.mask && flag_mask)
       {
-	for (i=0;i<output.npixels;i++)  output.mask[i] |= BADPIX_FLAT_MASK;
+        // Bug fix from Gary Bernstein
+	//for (i=0;i<output.npixels;i++)  output.mask[i] |= BADPIX_FLAT_MASK;
+        output.mask[i] |= BADPIX_FLAT_MASK;
       }
-
   }
 
   /* read the bias image */
@@ -614,37 +433,26 @@ int mkbpm(int argc,char *argv[])
     for(j=0;j<nbias;j++){
 
       destroy_desimage(&bias);
-
       sprintf(bias.name,"%s",biasnames[j]);
-
       rd_desimage(&bias,READONLY,flag_verbose);
 
       if (flag_verbose) {
-
 	sprintf(event,"Processing bias: %s",bias.name);
-
 	reportevt(flag_verbose,STATUS,1,event);
-
       }
 
-    /* check flat image */
+      /* check bias image */
 
       if (fits_movabs_hdu(bias.fptr,1,&hdutype,&status)) {
-
 	sprintf(event,"Moving to HDU=1 failed: %s",bias.name);
-
 	reportevt(flag_verbose,STATUS,5,event);
-
 	printerror(status);
-
       }
 
       headercheck(&bias,"NOCHECK",&ccdnum,"DESMKBCR",flag_verbose);
 
       if(initialize==1){
-
 	output.npixels=bias.npixels;
-
 	output.nfound=bias.nfound;
 
 	for (i=0;i<output.nfound;i++) output.axes[i]=bias.axes[i];
@@ -652,11 +460,8 @@ int mkbpm(int argc,char *argv[])
 	output.mask=(short *)calloc(output.npixels,sizeof(short));
 
 	if (output.mask==NULL) {
-
 	  reportevt(flag_verbose,STATUS,5,"Calloc of output.mask failed");
-
 	  return(1);
-
 	}
 
 	for (i=0;i<output.npixels;i++) {
@@ -689,26 +494,19 @@ int mkbpm(int argc,char *argv[])
       }
 
       if(bias.mask && flag_mask){
-	
-	for (i=0;i<output.npixels;i++) {
-	  output.mask[i] |= BADPIX_BIAS_MASK;
-	}
-	
+        // Bug fix from Gary Bernstein
+	//for (i=0;i<output.npixels;i++) {
+	//  output.mask[i] |= BADPIX_BIAS_MASK;
+	//}
+        output.mask[i] |= BADPIX_BIAS_MASK;
       }
-      
     }
-    
   }
 
-
   if (flag_edgemask)
-
     {	
-
       for (i=0;i<output.npixels;i++) {
-
 	xpos = i%output.axes[0] ;
-
 	ypos = i/output.axes[0] ;
 
 	if (xpos<edgesize || ypos<edgesize) output.mask[i] |= BADPIX_EDGE;
@@ -716,7 +514,6 @@ int mkbpm(int argc,char *argv[])
 	if (abs(xpos-output.axes[0])<=edgesize || abs(ypos-output.axes[1])<=edgesize) output.mask[i] |= BADPIX_EDGE;
 
       }
-
     }
 
   //Flag bad pixels based on majority logic
@@ -802,7 +599,7 @@ int mkbpm(int argc,char *argv[])
     }
 	  
   printf("CCDNUM=%i\n",ccdnum);
-  nbadcol = badcol("funky_column.lis",ccdnum,output);
+  nbadcol = badcol("funky_column.lst",ccdnum,output);
   //Count bad pixels by type
   for (j=0;j<16;++j) kount[j] = 0;
   badpix = 0;
@@ -885,7 +682,7 @@ int mkbpm(int argc,char *argv[])
 
   //This diagnostic print assumes particular values for BADPIX_*
   printf("Flat low=%i (%i < %6.3f)\n",kount[0],num_flat_min,min);
-  printf("Flat high=%i (%i > %6.3f \n",kount[1],num_flat_max,max);
+  printf("Flat high=%i (%i > %6.3f)\n",kount[1],num_flat_max,max);
   printf("Flat mask=%i \n",kount[2]);
   printf("Bias hot=%i (%i > %7.2f)\n",kount[3],num_bias_hot,biashot);
   printf("Bias warm=%i (%i > %7.2f)\n",kount[4],num_bias_warm,biaswarm);
@@ -919,33 +716,23 @@ int mkbpm(int argc,char *argv[])
   */
 
   /* ************************************************************ */
-
   /* *************** Test bpm against template ****************** */
-
   /* ************************************************************ */
 
   if (flag_image_compare) {
 
     /*  Read template image */
-
     rd_desimage(&template,READONLY,flag_verbose);
 
     /* check image for ccdnumber */
-
     headercheck(&template,"NOCHECK",&ccdnum,"DESMKBPM",flag_verbose);
-
     template.image=template.varim=NULL; 
 
     /* make sure that image pointers are NULL */
-
     if (output.image!=NULL || template.image!=NULL) {
-
       sprintf(event,"Image and weight map pointers must be NULLed to compare masks");
-
       reportevt(flag_verbose,STATUS,5,event);
-
       return(1);
-
     }
 
     rms=maxdev=offset=0.0;
@@ -958,74 +745,41 @@ int mkbpm(int argc,char *argv[])
 
     /* issue STATUS events according to differences measured */
 
-    
-
     /* ***************************************************** */
-
     /* ***************************************************** */
-
     /* ***************************************************** */
 
   }
 
-  
-
-
-
   /* *********************** */	
-
   /* **** SAVE RESULTS ***** */
-
   /* *********************** */
-
-  
 
   /* make sure path exists for new image */
 
   if (mkpath(output.name,flag_verbose)) {
-
     sprintf(event,"Failed to create path to file: %s",output.name+1);
-
     reportevt(flag_verbose,STATUS,5,event);
-
     return(1);
-
   }
 
   else {
-
     sprintf(event,"Created path to file: %s",output.name+1);
-
     reportevt(flag_verbose,STATUS,1,event);
-
   }
-
-  
 
   /* create the file */
-
   if (fits_create_file(&output.fptr,output.name,&status)) {
-
     sprintf(event,"Creating file failed: %s",output.name);
-
     reportevt(flag_verbose,STATUS,5,event);
-
     printerror(status);
-
   }
 
-  
-
   /* create image HDU */
-
   if (fits_create_img(output.fptr,USHORT_IMG,2,output.axes,&status)) {
-
     sprintf(event,"Creating image failed: %s",output.name);
-
     reportevt(flag_verbose,STATUS,5,event);
-
     printerror(status);
-
   }
 
   
@@ -1033,29 +787,19 @@ int mkbpm(int argc,char *argv[])
   /* write the corrected image*/
 
   if (fits_write_img(output.fptr,TUSHORT,1,output.npixels,output.mask,
-
 		     &status))  {
-
     sprintf(event,"Writing image failed: %s",output.name);
-
     reportevt(flag_verbose,STATUS,5,event);
-
     printerror(status);
-
   }
 
   
 
   if (fits_write_key_lng(output.fptr,"CCDNUM",ccdnum,
-
 			 "CCD Number",&status)) {
-
     sprintf(event,"Setting CCDNUM=%d failed: %s",ccdnum,output.name);
-
     reportevt(flag_verbose,STATUS,5,event);
-
     printerror(status);
-
   }
 
   
@@ -1065,19 +809,13 @@ int mkbpm(int argc,char *argv[])
   /* get system time */
 
   tm=time(NULL);
-
   sprintf(comment,"%s",asctime(localtime(&tm)));
-
   comment[strlen(comment)-1]=0;
 
   if (fits_write_key_str(output.fptr,"DESMKBPM",comment,"bad pixel map created",&status)) {
-
     sprintf(event,"Writing DESMKBPM keyword failed: %s",output.name);
-
     reportevt(flag_verbose,STATUS,5,event);
-
     printerror(status);
-
   }
 
   sprintf(longcomment,"DESDM:");
@@ -1087,23 +825,15 @@ int mkbpm(int argc,char *argv[])
   if (flag_verbose) reportevt(flag_verbose,STATUS,1,longcomment);
 
   if (fits_write_comment(output.fptr,longcomment,&status)) {
-
     sprintf(event,"Writing longcomment failed: %s",output.name);
-
     reportevt(flag_verbose,STATUS,5,event);
-
     printerror(status);
-
   }
 
   if (fits_write_key_str(output.fptr,"DES_EXT","MASK","Extension type",&status)) {
-
     sprintf(event,"Writing DES_EXT=MASK failed: %s",output.name);
-
     reportevt(flag_verbose,STATUS,5,event);
-
     printerror(status);
-
   }
 
   
@@ -1111,29 +841,22 @@ int mkbpm(int argc,char *argv[])
   /* close the corrected image */
 
   if (fits_close_file(output.fptr,&status)) {
-
     sprintf(event,"File close failed: %s",output.name);
-
     reportevt(flag_verbose,STATUS,5,event);
-
     printerror(status);
-
   }
 
   if (flag_verbose) {
-
     sprintf(event,"Closed %s: 2D ( %ld X %ld )",
-
 	    &(output.name[flag_output]),output.axes[0],output.axes[1]);	  
 
     reportevt(flag_verbose,STATUS,1,event);
-
   }
 
   return(0);
-
 }
 
+//ADW: Should this be named "funkycol"?
 int badcol(char *fname,int ccdnum,desimage out)
 {
   FILE* fin;
@@ -1152,7 +875,9 @@ int badcol(char *fname,int ccdnum,desimage out)
 
   const float MINTHRES = -5.0;
   for (i=1;i<=2048;++i) dev[i]=0.0;
-  fin = fopen("funky_column.lst","r");
+  //ADW: This should respect input file name
+  //fin = fopen("funky_column.lst","r");
+  fin = fopen(fname,"r");
   if (fin > 0) 
     {
       // If successful, read the funky columns
@@ -1263,7 +988,6 @@ int badcol(char *fname,int ccdnum,desimage out)
 }
 
 int main(int argc, char *argv[])
-
 {
   return(mkbpm(argc,argv));
 };
